0400f8956a2bf1656f0d55ab37ad1e0eb59c2f9e
w.alexander <will_alexander@me.com>
2026-03-13 21:49:27 +0100

    Finish 3.2a

--- src/test/java/org/example/phase3exploration/FileIOTest.java
+++ src/test/java/org/example/phase3exploration/FileIOTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.io.TempDir;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,5 +20,45 @@ public class FileIOTest {
         Files.writeString(tempDir.resolve("test.txt"), "Hello, World!");
         String file = Files.readString(tempDir.resolve("test.txt"));
         assertThat(file).isEqualTo("Hello, World!");
+    }
+
+    @Test
+    void shouldWriteAndReadLines() throws IOException {
+        List<String> lines = List.of("Line 1", "Line 2", "Line 3");
+        Files.write(tempDir.resolve("test.txt"), lines);
+        List<String> readLines = Files.readAllLines(tempDir.resolve("test.txt"));
+        assertThat(readLines).isEqualTo(lines);
+    }
+
+    @Test
+    void shouldDeleteFile() throws IOException {
+        assertThat(Files.exists(tempDir.resolve("test.txt"))).isFalse();
+        Files.writeString(tempDir.resolve("test.txt"), "Hello, World!");
+        assertThat(Files.exists(tempDir.resolve("test.txt"))).isTrue();
+        Files.delete(tempDir.resolve("test.txt"));
+        assertThat(Files.exists(tempDir.resolve("test.txt"))).isFalse();
+    }
+
+    @Test
+    void shouldCreateDirectories() throws IOException {
+        Path dir = tempDir.resolve("data");
+        Files.createDirectories(dir);
+        Path subdir = dir.resolve("subdir");
+        Files.createDirectories(subdir);
+        assertThat(Files.exists(subdir)).isTrue();
+    }
+
+    @Test
+    void shouldListFiles() throws IOException {
+        Files.writeString(tempDir.resolve("test1.txt"), "Hello, World!");
+        Files.writeString(tempDir.resolve("test2.txt"), "Goodbye, World!");
+        Files.writeString(tempDir.resolve("test.json"), "{ \"key\": \"value\" }");
+        try (var files = Files.list(tempDir)) {
+            assertThat(files).hasSize(3);
+        }
+        try (var files = Files.list(tempDir)) {
+            assertThat(files.filter(path -> path.getFileName().toString().endsWith(".json"))).hasSize(1);
+        }
+        ;
     }
 }
